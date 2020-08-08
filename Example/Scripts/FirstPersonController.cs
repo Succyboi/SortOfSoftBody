8889f75382c6100e1919a0ecfc238485c7727bef
Pelle Bruinsma <Spookezatemyneighbours@gmail.com>
2020-08-08 15:18:47 +0200

    Added unity files

---
+++ Example/Scripts/FirstPersonController.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    public GameObject head;
+    public float movementSpeed;
+    public float mouseSens;
+    public LayerMask holdingMask;
+    public float holdingSpeed;
+    public float holdingDistance;
+    public float throwingForce;
+    public float throwingCooldown;
+
+    private Vector3 direction;
+    private Rigidbody rb;
+    private float horizontalRot;
+    private float verticalRot;
+    private GameObject holdingObject;
+    private Rigidbody holdingObjectRb;
+    private float throwingCooldownTimer;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        horizontalRot += Input.GetAxis("Mouse X") * mouseSens;
+        verticalRot -= Input.GetAxis("Mouse Y") * mouseSens;
+
+        verticalRot = Mathf.Clamp(verticalRot, -90, 90);
+        
+        //aim
+        transform.rotation = Quaternion.Euler(0, horizontalRot, 0);
+        head.transform.localRotation = Quaternion.Euler(verticalRot,
+            0,
+            0);
+
+        if (Input.GetAxis("Fire1") > 0)
+        {
+            if (holdingObject == null && throwingCooldownTimer < 0)
+            {
+                PickUpProp();
+            }
+            Screen.lockCursor = true;
+        }
+        else
+        {
+            if (holdingObject != null)
+            {
+                DropProp();
+            }
+        }
+
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Screen.lockCursor = false;
+        }
+
+        if (Input.GetAxis("Fire2") > 0)
+        {
+            if (holdingObject != null && throwingCooldownTimer < 0)
+            {
+                holdingObjectRb.AddForce(head.transform.forward * throwingForce * holdingObjectRb.mass, ForceMode.Impulse);
+                DropProp();
+
+                throwingCooldownTimer = throwingCooldown;
+            }
+        }
+
+        throwingCooldownTimer -= Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(transform.TransformDirection(direction) * movementSpeed * Time.deltaTime + transform.position);
+
+        if(holdingObject != null)
+        {
+            holdingObjectRb.MovePosition(holdingObject.transform.position + (Vector3.ClampMagnitude((head.transform.forward * holdingDistance + head.transform.position) - holdingObject.transform.position, 1) * holdingSpeed * Time.deltaTime));
+
+            //holdingObjectRb.AddForce(((head.transform.forward * holdingDistance + head.transform.position) - holdingObject.transform.position) * holdingSpeed, ForceMode.Acceleration);
+            holdingObjectRb.velocity = Vector3.zero;
+        }
+    }
+
+    private void PickUpProp()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(head.transform.position, head.transform.forward, out hit, holdingDistance * 2, holdingMask))
+        {
+            if (hit.transform.CompareTag("Prop"))
+            {
+                holdingObject = hit.transform.gameObject;
+                holdingObjectRb = holdingObject.GetComponent<Rigidbody>();
+            }
+        }
+    }
+
+    private void DropProp()
+    {
+        holdingObject = null;
+        holdingObjectRb = null;
+    }
+}
---
+++ Stupid/SortOfSoftBody/SoftBody.cs
@@ -0,0 +1,237 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+#region Custom Editor
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(SoftBody)), CanEditMultipleObjects]
+public class SoftBodyEditor : Editor
+{
+	SerializedProperty impactForceMultiplier;
+	SerializedProperty impactForceOffset;
+	SerializedProperty springForce;
+	SerializedProperty damping;
+	SerializedProperty minimumCollisionForce;
+	SerializedProperty lastCollisionForce;
+	SerializedProperty deformCollider;
+
+	bool showDescription;
+	bool showExperimental;
+
+	void OnEnable()
+	{
+		impactForceMultiplier = serializedObject.FindProperty("impactForceMultiplier");
+		impactForceOffset = serializedObject.FindProperty("impactForceOffset");
+		springForce = serializedObject.FindProperty("springForce");
+		damping = serializedObject.FindProperty("damping");
+		deformCollider = serializedObject.FindProperty("deformCollider");
+		minimumCollisionForce = serializedObject.FindProperty("minimumCollisionForce");
+		lastCollisionForce = serializedObject.FindProperty("lastCollisionForce");
+	}
+
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update();
+
+		EditorGUILayout.LabelField("Settings", EditorStyles.boldLabel);
+
+		Rect rect1 = EditorGUILayout.GetControlRect(false, 1);
+		rect1.height = 1;
+		EditorGUI.DrawRect(rect1, new Color(0.5f, 0.5f, 0.5f, 1));
+
+		EditorGUILayout.Slider(impactForceMultiplier, -2, 2);
+
+		EditorGUILayout.Slider(impactForceOffset, 0, 1);
+
+		EditorGUILayout.Slider(springForce, 0, 100);
+
+		EditorGUILayout.Slider(damping, 0, 100);
+
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.PrefixLabel("Minimum Collision Force");
+		minimumCollisionForce.floatValue = EditorGUILayout.FloatField(minimumCollisionForce.floatValue);
+		EditorGUILayout.EndHorizontal();
+
+		EditorGUILayout.BeginHorizontal();
+		EditorGUILayout.PrefixLabel("Last Collision Force");
+		EditorGUILayout.LabelField(lastCollisionForce.floatValue.ToString());
+		EditorGUILayout.EndHorizontal();
+
+		Rect rect2 = EditorGUILayout.GetControlRect(false, 1);
+		rect2.height = 1;
+		EditorGUI.DrawRect(rect2, new Color(0.5f, 0.5f, 0.5f, 1));
+
+		showExperimental = EditorGUILayout.Foldout(showExperimental, "Show Experimental Settings");
+
+        if (showExperimental)
+        {
+			EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.PrefixLabel("Deform Collider");
+			deformCollider.boolValue = EditorGUILayout.Toggle(deformCollider.boolValue);
+			EditorGUILayout.EndHorizontal();
+		}
+
+		showDescription = EditorGUILayout.Foldout(showDescription, "Description");
+
+		if (showDescription)
+		{
+			EditorGUILayout.HelpBox("This script deforms the mesh of the gameobject it is attached to on impact.", MessageType.Info);
+		}
+
+		serializedObject.ApplyModifiedProperties();
+	}
+}
+
+#endif
+#endregion
+
+[RequireComponent(typeof(MeshFilter)), RequireComponent(typeof(MeshCollider)), AddComponentMenu("Physics/SoftBody")]
+public class SoftBody : MonoBehaviour {
+
+	public float impactForceMultiplier = 0.1f;
+	public float impactForceOffset = 0;
+	public float springForce = 20f;
+	public float damping = 1;
+	public bool deformCollider;
+
+	public float minimumCollisionForce = 10;
+	public float lastCollisionForce;
+
+	Mesh filterMesh;
+	Vector3[] filterOriginalVertices, filterDisplacedVertices;
+	Vector3[] filterVertexVelocities;
+
+	MeshCollider colliderComponent;
+	Mesh colliderMesh;
+	Vector3[] colliderOriginalVertices, colliderDisplacedVertices;
+	Vector3[] colliderVertexVelocities;
+
+	float uniformScale = 1f;
+
+	void Start () {
+		filterMesh = GetComponent<MeshFilter>().mesh;
+		filterOriginalVertices = filterMesh.vertices;
+		filterDisplacedVertices = new Vector3[filterOriginalVertices.Length];
+		for (int i = 0; i < filterOriginalVertices.Length; i++) {
+			filterDisplacedVertices[i] = filterOriginalVertices[i];
+		}
+		filterVertexVelocities = new Vector3[filterOriginalVertices.Length];
+
+        if (deformCollider)
+        {
+			colliderComponent = GetComponent<MeshCollider>();
+			colliderComponent.sharedMesh = (Mesh)Instantiate(colliderComponent.sharedMesh);
+			colliderMesh = colliderComponent.sharedMesh;
+			colliderOriginalVertices = colliderMesh.vertices;
+			colliderDisplacedVertices = new Vector3[colliderOriginalVertices.Length];
+			for (int i = 0; i < colliderOriginalVertices.Length; i++)
+			{
+				colliderDisplacedVertices[i] = colliderOriginalVertices[i];
+			}
+			colliderVertexVelocities = new Vector3[colliderOriginalVertices.Length];
+		}
+	}
+
+	void Update () {
+		uniformScale = transform.localScale.x;
+		for (int i = 0; i < filterDisplacedVertices.Length; i++) {
+			UpdateFilterVertex(i);
+		}
+		filterMesh.vertices = filterDisplacedVertices;
+		filterMesh.RecalculateNormals();
+
+		if (deformCollider)
+		{
+			if(colliderMesh == null)
+            {
+				colliderComponent = GetComponent<MeshCollider>();
+				colliderComponent.sharedMesh = (Mesh)Instantiate(colliderComponent.sharedMesh);
+				colliderMesh = colliderComponent.sharedMesh;
+				colliderOriginalVertices = colliderMesh.vertices;
+				colliderDisplacedVertices = new Vector3[colliderOriginalVertices.Length];
+				for (int i = 0; i < colliderOriginalVertices.Length; i++)
+				{
+					colliderDisplacedVertices[i] = colliderOriginalVertices[i];
+				}
+				colliderVertexVelocities = new Vector3[colliderOriginalVertices.Length];
+			}
+
+			uniformScale = transform.localScale.x;
+			for (int i = 0; i < colliderDisplacedVertices.Length; i++)
+			{
+				UpdateColliderVertex(i);
+			}
+			colliderMesh.vertices = colliderDisplacedVertices;
+			colliderMesh.RecalculateNormals();
+
+			colliderComponent.sharedMesh = colliderMesh;
+		}
+	}
+
+	void UpdateFilterVertex (int i) {
+		Vector3 velocity = filterVertexVelocities[i];
+		Vector3 displacement = filterDisplacedVertices[i] - filterOriginalVertices[i];
+		displacement *= uniformScale;
+		velocity -= displacement * springForce * Time.deltaTime;
+		velocity *= 1f - damping * Time.deltaTime;
+		filterVertexVelocities[i] = velocity;
+		filterDisplacedVertices[i] += velocity * (Time.deltaTime / uniformScale);
+	}
+
+	void UpdateColliderVertex(int i)
+	{
+		Vector3 velocity = colliderVertexVelocities[i];
+		Vector3 displacement = colliderDisplacedVertices[i] - colliderOriginalVertices[i];
+		displacement *= uniformScale;
+		velocity -= displacement * springForce * Time.deltaTime;
+		velocity *= 1f - damping * Time.deltaTime;
+		colliderVertexVelocities[i] = velocity;
+		colliderDisplacedVertices[i] += velocity * (Time.deltaTime / uniformScale);
+	}
+
+	public void AddDeformingForce (Vector3 point, float force) {
+		point = transform.InverseTransformPoint(point);
+		for (int i = 0; i < filterDisplacedVertices.Length; i++) {
+			AddForceToFilterVertex(i, point, force);
+
+            if (deformCollider)
+            {
+				AddForceToColliderVertex(i, point, force);
+			}
+		}
+	}
+
+	void AddForceToFilterVertex (int i, Vector3 point, float force) {
+		Vector3 pointToVertex = filterDisplacedVertices[i] - point;
+		pointToVertex *= uniformScale;
+		float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
+		float velocity = attenuatedForce * Time.deltaTime;
+		filterVertexVelocities[i] += pointToVertex.normalized * velocity;
+	}
+
+	void AddForceToColliderVertex(int i, Vector3 point, float force)
+	{
+		Vector3 pointToVertex = colliderDisplacedVertices[i] - point;
+		pointToVertex *= uniformScale;
+		float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
+		float velocity = attenuatedForce * Time.deltaTime;
+		colliderVertexVelocities[i] += pointToVertex.normalized * velocity;
+	}
+
+	private void OnCollisionEnter(Collision collision)
+    {
+		float collisionForce = collision.impulse.magnitude / Time.fixedDeltaTime;
+
+		lastCollisionForce = collisionForce;
+
+		if(collisionForce > minimumCollisionForce)
+        {
+			foreach (ContactPoint cp in collision.contacts)
+			{
+				Vector3 point = cp.point;
+				point += cp.normal * impactForceOffset;
+				AddDeformingForce(point, -impactForceMultiplier * collisionForce / collision.contactCount);
+			}
+		}
+    }
+}
